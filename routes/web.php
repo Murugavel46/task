b38f11e23a97e52034bccb6179b6ef6e2d904682
Murugavel46 <murugavelkolanthasamy131001@gmail.com>
2024-10-26 22:46:38 +0530

    Group Routing ,Middleware Hande session

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,27 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\AuthController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('register', 'auth.register')->middleware('guest')->name('register');
-Route::post('/register', [AuthController::class, 'register'])->middleware('guest')->name('register.s');
+
+Route::middleware('guest')->group(function () {
+    Route::view('register', 'auth.register')->name('register');
+    Route::post('/register', [AuthController::class, 'register'])->name('register.s');
+    Route::view('login', 'auth.login')->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('loginsubmit');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/change-password', [AuthController::class, 'showChangePasswordForm'])->name('change_password.form');
+    Route::post('/change-password', [AuthController::class, 'changePassword'])->name('change_password');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 
 
-Route::view('login', 'auth.login')->middleware('guest')->name('login'); 
-Route::post('/login', [AuthController::class, 'login'])->middleware('guest')->name('loginsubmit');
-
-
-Route::get('/change-password', [AuthController::class, 'showChangePasswordForm'])->name('change_password.form');
-Route::post('/change-password', [AuthController::class, 'changePassword'])->name('change_password');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::post('/logout',[AuthController::class,'logout'])->name('logout');
 Route::get('/welcome', function () {
     return view('auth.welcome');
-})->name('welcome');
+})->middleware('auth')->name('welcome');
